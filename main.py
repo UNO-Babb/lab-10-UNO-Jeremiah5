75a3345486b898501b0f9427916c659de0e1eeac
UNO-Jeremiah5 <Jeremiahsanchez@unomaha.edu>
2025-04-16 15:18:54 +0000

    ALmost done

--- main.py
+++ main.py
@@ -1,4 +1,48 @@
 #MapPlot.py
-#Name:
-#Date:
-#Assignment:
+#Name:Jeremiah Sanchez 
+#Date: 16 April 2025
+#Assignment: Los Angeles Lakers in 5
+
+import astronauts
+mission = astronauts.get_mission()
+
+all_mission = len(mission)
+
+vehicle_list = []
+year_list = []
+
+for spot in range(all_mission):
+    vehicle = mission[spot]["Mission"]["Vechicles"]["Orbit"]
+    year = mission[spot]["Mission"]["Year"]
+    if year > 2010: #vehicle == "STS-124":
+        print(year)
+        print(vehicle)
+        vehicle_list.append(vehicle)
+        year_list.append(year)
+
+print(vehicle_list, year_list)
+
+import matplotlib.pyplot as plt
+plt.plot(vehicle_list, year_list)
+plt.xlabel('Mission')
+plt.ylabel('Year')
+plt.savefig("output")
+
+
+
+
+
+
+
+"""
+
+
+if total_score > 10:
+    scores.append(total_score)
+    years.append(year)
+
+
+
+    plt.plot(years, scores, 'ro')
+
+"""
